931acd7750ce77bc587723022b7db16c3694d088
Xuan Huy <xuanhuy900@gmail.com>
2020-10-23 22:47:41 +0700

    config enviroment, connect database

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,29 @@
-package cmd
+package main
+import(
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"os"
+	"shopgo/db"
+	"shopgo/model"
+)
+
+func main() {
+	var cfg model.Config
+	loadConfig(&cfg)
+
+	sql := new(db.SQL)
+	sql.Connect(cfg)
+
+	defer sql.Close()
+
+}
+func loadConfig(cfg *model.Config) {
+	f, err := os.Open("../env.dev.yml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(cfg)
+}
